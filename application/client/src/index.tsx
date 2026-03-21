18791dc2f82f0028628ada4874e22b2277982606
Shion Ichikawa <shion1305@gmail.com>
2026-03-21 12:38:19 +0900

    Revert "🧹 chore: remove unused redux wiring from client bootstrap (#20)" (#25)
    This reverts commit 94a7a7b7312c918bfa6c26a6896cf46f6a8f5d07.

--- application/client/src/index.tsx
+++ application/client/src/index.tsx
@@ -1,10 +1,14 @@
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router";
 
 import { AppContainer } from "@web-speed-hackathon-2026/client/src/containers/AppContainer";
+import { store } from "@web-speed-hackathon-2026/client/src/store";
 
 createRoot(document.getElementById("app")!).render(
-  <BrowserRouter>
-    <AppContainer />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <AppContainer />
+    </BrowserRouter>
+  </Provider>,
 );
